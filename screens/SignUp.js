10c5f6308e724c0a200726bdde322f17f6c0f84e
Fernando Leniz <ferleniz93@gmail.com>
2021-10-08 00:35:21 -0300

    background gradient

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react"
 import { StyleSheet, Text, View, TextInput, ScrollView,TouchableOpacity } from "react-native"
 import Header from "../components/Header"
+import { LinearGradient } from 'expo-linear-gradient';
 
 const SignUp = (props) => {
   const [newUser, setNewUser] = useState({
@@ -25,36 +26,43 @@ const SignUp = (props) => {
   return (
     <ScrollView>
         <Header {...props} />
-      <View style={styles.container}>
+      <LinearGradient colors={[' rgba(47,144,176,1)', 'rgba(48,106,154,1)', 'rgba(49,75,136,1)','rgba(49,25,109,1)']}
+        start={{x: 0, y: 0.5}}
+        end={{x: 1, y: 1}} style={styles.container}>
         <Text style={styles.title}>REGISTRATE</Text>
         <TextInput
           style={styles.input}
           placeholder=" Nombre"
           value={newUser.firstName}
           onChangeText={(e) => changeValueInput(e, "firstName")}
+          placeholderTextColor={'white'}
         />
         <TextInput
           style={styles.input}
           placeholder="Apellido"
           value={newUser.lastName}
           onChangeText={(e) => changeValueInput(e, "lastName")}
+          placeholderTextColor={'white'}
         />
         <TextInput
           style={styles.input}
           placeholder="Email"
           value={newUser.eMail}
           onChangeText={(e) => changeValueInput(e, "eMail")}
+          placeholderTextColor={'white'}
         />
         <TextInput
           style={styles.input}
           placeholder="Contraseña"
           value={newUser.password}
           onChangeText={(e) => changeValueInput(e, "password")}
+          placeholderTextColor={'white'}
         />
         <TextInput
           style={styles.input}
           placeholder="Confirmar contraseña"
           onChangeText={(e) => changeValueInput(e, "check password")}
+          placeholderTextColor={'white'}
         />
         <View>
         <TouchableOpacity
@@ -62,10 +70,10 @@ const SignUp = (props) => {
             style={styles.button}
             activeOpacity={0.7}
           >
-            <Text style={styles.text}>Sign Up</Text>
+            <Text style={styles.text}>Registrarme</Text>
           </TouchableOpacity>
         </View>
-      </View>
+      </LinearGradient>
     </ScrollView>
   )
 }
@@ -76,7 +84,7 @@ const styles = StyleSheet.create({
   container: {
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor:'radial-gradient(circle, rgba(47,144,176,1) 0%, rgba(48,106,154,1) 55%, rgba(49,75,136,1) 73%, rgba(49,25,109,1) 100%);'
+    paddingBottom:24
   },
   title:{
       fontSize:40,
@@ -102,12 +110,14 @@ const styles = StyleSheet.create({
     borderRadius: 4,
     elevation: 3,
     marginTop: 20,
-    backgroundColor: "#2f2f6b",
+    backgroundColor: "transparent",
     width: "100%",
     height: 50,
     zIndex: 1,
     marginBottom:10,
-    padding:10
+    padding:10,
+    borderBottomColor: 'white',
+    borderBottomWidth: 2,
   },
   text: {
     fontSize: 16,
