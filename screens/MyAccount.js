3a9e1078e60931d7cce2678d39a6fe3c64e8d1e0
enrique-dc93 <51374100+enrique-dc93@users.noreply.github.com>
2021-10-13 17:00:04 -0300

    edit and complete sin finalizar

--- screens/MyAccount.js
+++ screens/MyAccount.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import {
   StyleSheet,
   Text,
@@ -14,97 +14,205 @@ import Header from "../components/Header"
 import { connect } from "react-redux"
 import usersAction from "../redux/actions/usersAction"
 import UserPurchase from "../components/UserPurchase"
+import { showMessage } from "react-native-flash-message"
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view"
 
 
 const MyAccount = (props) => {
-
+    const { dni , id, getData, token, firstName, lastName, editDataUser } = props
     const [view, setView] = useState(true)
-    const [dataUser, setDataUser] = useState({
-        firstName: "",
-        lastName: "",
-        city: "",
-        zipCode: "", 
-        address: "", 
-        optional: "", 
-        phone: ""
-    })
+    
+    let completeAccount = !isNaN(dni)
+    let initialDataUser = completeAccount ? { firstName: "", lastName: "", city: "", zipCode: "", address: "", optional: "", phone: "" }
+        : { dni: null, city: "", zipCode: "", address: "", optional: "", phone: "" } 
+    const [dataUser, setDataUser] = useState(initialDataUser)
+    const [errorsValidation, setErrorsValidation] = useState({})
+    const[loading,setLoading]=useState(true)
 
-    const handleInput = (e) => {
 
+    useEffect( () => {
+        const getDataUser = async () => {
+            try{
+                let extraData = await getData(id, token)                
+                setDataUser( {...extraData, firstName, lastName } )
+            } catch(err) {
+                showMessage({
+                    "message":err.message,
+                    "type":"danger"
+                })
+            }
+        }
+        if(completeAccount) getDataUser()
+        // setTimeout(()=>{
+            setLoading(!loading)  
+        // },500)
+        //eslint-disable-next-line react-hooks/exhaustive-deps
+    },[])
+
+    const inputHandler = e => {
+        setDataUser({ ...dataUser, [e.target.name]: e.target.value})
+    }
+
+    const keyPressHandler = e => {
+        if(e.key === "Enter") updateDataUser()
+    }
+
+    const updateDataUser = async (e) => {
+        e.preventDefault()
+        try{
+            let response = await editDataUser(id, completeAccount, token, dataUser)
+            console.log(response)
+            if(response.success){ 
+                if(!completeAccount){
+                    showMessage({
+                        "message":"Datos Actualizados con éxito ya puedes comprar",
+                        "type": "success"
+                    })
+                    completeAccount = true
+                } else {
+                    showMessage({
+                        "message":"Datos Actualizados con éxito",
+                        "type": "success"
+                    })
+                }
+            }
+        } catch(error) {
+            if (typeof error === 'string' || error === "DNI en uso"){
+            } else if (Array.isArray(error)){
+                let errors = {};
+                error.forEach(err=> {
+                    errors[err.path[0]] = err.message;
+                })
+                setErrorsValidation(errors);
+            } else {
+                showMessage({
+                    "message":"Error de Conexión",
+                    "type": "danger"
+                })
+            }
+        }
     }
 
 
     return (
-        <ImageBackground source={{uri: 'https://i.postimg.cc/ryjKWhwG/luke-chesser-p-Jad-Qetz-Tk-I-unsplash.jpg'}} style={styles.viewContainerHome}>
-            <View>
-                <Header {...props} />
+        <KeyboardAwareScrollView
+            // style={{ backgroundColor: '#4c69a5' }}
+            resetScrollToCoords={{ x: 0, y: 0 }}
+            contentContainerStyle={styles.container}
+            scrollEnabled={false}
+        >
+            <ImageBackground source={{uri: 'https://i.postimg.cc/ryjKWhwG/luke-chesser-p-Jad-Qetz-Tk-I-unsplash.jpg'}} style={styles.viewContainerHome}>
                 <View>
-                    <Text style={styles.title}>{view ? "Puedes editar estos datos de tu cuenta" : "Historial de compras"}</Text>
-                    <TouchableOpacity style={styles.menu}>
-                        <Text style={styles.textMenu} onPress={()=>setView(false)}>Mis compras</Text>
-                        <Text style={styles.textMenu} onPress={()=>setView(true)}>Mis datos</Text>
-                    </TouchableOpacity>
-                    {view ?
-                    <View style={styles.inputToEdit}>
-                        <TextInput 
-                            style={styles.input}
-                            placeholder="Nombre"
-                            defaultValue={dataUser.firstName}
-                            onChangeText={handleInput}
-                            placeholderTextColor={"white"}
-                        />
-                        <TextInput 
-                            style={styles.input}
-                            placeholder="Apellido"
-                            defaultValue={dataUser.lastName}
-                            onChangeText={handleInput}
-                            placeholderTextColor={"white"}
-                        />
-                        <TextInput 
-                            style={styles.input}
-                            placeholder="Teléfono"
-                            defaultValue={dataUser.city}
-                            onChangeText={handleInput}
-                            placeholderTextColor={"white"}
-                        />
-                        <TextInput 
-                            style={styles.input}
-                            placeholder="Ciudad"
-                            defaultValue={dataUser.zipCode}
-                            onChangeText={handleInput}
-                            placeholderTextColor={"white"}
-                        />
-                        <TextInput 
-                            style={styles.input}
-                            placeholder="Código postal"
-                            defaultValue={dataUser.address}
-                            onChangeText={handleInput}
-                            placeholderTextColor={"white"}
-                        />
-                        <TextInput 
-                            style={styles.input}
-                            placeholder="Dirección"
-                            defaultValue={dataUser.city}
-                            onChangeText={handleInput}
-                            placeholderTextColor={"white"}
-                        />
-                        <TextInput 
-                            style={styles.input}
-                            placeholder="Opcional"
-                            defaultValue={dataUser.optional}
-                            onChangeText={handleInput}
-                            placeholderTextColor={"white"}
-                        />
-                        <TouchableOpacity style={styles.button}>
-                            <Text style={styles.textButton}>Actualizar</Text>
+                    <Header {...props} />
+                    <View>
+                        <Text style={styles.title}>
+                            {!view ? "Historial de compras" : completeAccount ? 
+                                "Puedes editar estos datos de tu cuenta" : "Completa tus datos para poder comprar"}
+                        </Text>
+                        <TouchableOpacity style={styles.menu}>
+                            <Text style={styles.textMenu} onPress={()=>setView(false)}>Mis compras</Text>
+                            <Text style={styles.textMenu} onPress={()=>setView(true)}>Mis datos</Text>
                         </TouchableOpacity>
-                    </View> :
-                    <UserPurchase id={props.id}/>}
+                        {view ?
+                        
+                            <View style={styles.inputToEdit}>
+                                {!completeAccount &&
+                                <View>
+                                    <Text style={styles.label}>DNI</Text>
+                                    <TextInput
+                                        style={styles.input}
+                                        placeholder="ej 44444444"
+                                        // defaultValue={dataUser.firstName}
+                                        onChangeText={inputHandler}
+                                        placeholderTextColor={"white"}
+                                        keyboardType={"numeric"}
+                                    
+                                    />
+                                </View>}                        
+                                {completeAccount &&
+                                <View>
+                                    <Text style={styles.label}>Nombre: </Text>
+                                    <TextInput
+                                        style={styles.input}
+                                        placeholder="ej Juan"
+                                        defaultValue={dataUser.firstName}
+                                        onChangeText={inputHandler}
+                                        placeholderTextColor={"white"}
+                                    />
+                                </View>}
+                                {completeAccount &&
+                                <View>
+                                    <Text style={styles.label}>Apellido: </Text>
+                                    <TextInput
+                                        style={styles.input}
+                                        placeholder="ej Garcia"
+                                        defaultValue={dataUser.lastName}
+                                        onChangeText={inputHandler}
+                                        placeholderTextColor={"white"}
+                                    />
+                                </View>}
+                                <View>
+                                    {completeAccount && <Text style={styles.label}>Teléfono: </Text>}
+                                    <TextInput
+                                        style={styles.input}
+                                        placeholder={!completeAccount ? "Teléfono" : "ej 114587427"}
+                                        defaultValue={dataUser.phone.toString()}
+                                        onChangeText={inputHandler}
+                                        placeholderTextColor={"white"}
+                                        keyboardType={"numeric"}
+                                    />
+                                </View>
+                                <View>
+                                    {completeAccount && <Text style={styles.label}>Ciudad: </Text>}
+                                    <TextInput
+                                        style={styles.input}
+                                        placeholder={!completeAccount ? "Ciudad" : "ej Maipu, Mendoza"}
+                                        defaultValue={dataUser.city}
+                                        onChangeText={inputHandler}
+                                        placeholderTextColor={"white"}
+                                    />
+                                </View>
+                                <View>
+                                    {completeAccount && <Text style={styles.label}>Código Postal: </Text>}
+                                    <TextInput
+                                        style={styles.input}
+                                        placeholder={!completeAccount ? "Código postal" : "ej 5501"}
+                                        defaultValue={dataUser.zipCode.toString()}
+                                        onChangeText={inputHandler}
+                                        placeholderTextColor={"white"}
+                                        keyboardType={"numeric"}
+                                    />
+                                </View>
+                                <View>
+                                    {completeAccount && <Text style={styles.label}>Dirección: </Text>}
+                                    <TextInput
+                                        style={styles.input}
+                                        placeholder={!completeAccount ? "Dirección" : "ej Salta 1234"}
+                                        defaultValue={dataUser.address}
+                                        onChangeText={inputHandler}
+                                        placeholderTextColor={"white"}
+                                    />
+                                </View>
+                                <View>
+                                    {completeAccount && <Text style={styles.label}>Opcional: </Text>}
+                                    <TextInput
+                                        style={styles.input}
+                                        placeholder={!completeAccount ? "Opcional" : "ej casa o depto/piso"}
+                                        defaultValue={dataUser.optional}
+                                        onChangeText={inputHandler}
+                                        placeholderTextColor={"white"}
+                                    />
+                                </View>
+                                <TouchableOpacity style={styles.button}>
+                                    <Text style={styles.textButton}>{completeAccount ? "Editar" : "Enviar!"}</Text>
+                                </TouchableOpacity>
+                            </View>
+                            : <UserPurchase id={props.id}/>}
+                    </View>
+                    
                 </View>
-                
-
-            </View>
-        </ImageBackground>
+            </ImageBackground>
+        </KeyboardAwareScrollView>
     )
 }
 
@@ -112,6 +220,7 @@ const mapStateToProps = (state) => {
     return {
         token: state.users.token,
         id: state.users.id,
+        dni: state.users.dni
     }
 }
 
